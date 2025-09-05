68e5d0c57362de0f0bea398414f1135adbf0f55c
Tachera Sasi <tacherasasi@gmail.com>
2025-09-05 12:47:50 +0300

    Implement file server with upload, download, and listing functionalities

---
+++ beam/server.go
@@ -0,0 +1,59 @@
+package beam
+
+import (
+    "fmt"
+    "net"
+    "net/http"
+    "os"
+    "io"
+)
+
+func StartServer() {
+	fs := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fs)
+
+    // list files
+    http.HandleFunc("/files", func(w http.ResponseWriter, r *http.Request) {
+        files, _ := os.ReadDir("./shared")
+        for _, f := range files {
+            fmt.Fprintln(w, f.Name())
+        }
+    })
+
+    // download
+    http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+        f, err := os.Open("./shared/" + r.URL.Query().Get("file"))
+        if err != nil {
+            http.Error(w, "File not found", 404)
+            return
+        }
+        defer f.Close()
+        io.Copy(w, f)
+    })
+
+    // upload
+    http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+        file, header, _ := r.FormFile("file")
+        defer file.Close()
+        out, _ := os.Create("./shared/" + header.Filename)
+        defer out.Close()
+        io.Copy(out, file)
+        fmt.Fprintln(w, "Uploaded")
+    })
+
+    ip := getLocalIP()
+    fmt.Println("Open http://" + ip + ":8080")
+    http.ListenAndServe(":8080", nil)
+}
+
+func getLocalIP() string {
+    addrs, _ := net.InterfaceAddrs()
+    for _, addr := range addrs {
+        if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                return ipnet.IP.String()
+            }
+        }
+    }
+    return "localhost"
+}
