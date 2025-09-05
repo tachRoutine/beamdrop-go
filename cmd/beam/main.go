95959979deeeb90f39173c9c4e87bdbee4c3870a
Tachera Sasi <tacherasasi@gmail.com>
2025-09-05 12:33:19 +0300

    Add QR code generation functionality and update dependencies

--- cmd/beam/main.go
+++ cmd/beam/main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/skip2/go-qrcode"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	
+}
+
+func generateQRCode(data string, filename string) error {
+	// Generate a QR code
+	qrCode, err := qrcode.New("ekilie.com", qrcode.Medium)
+	if err != nil {
+		return err
+	}
+
+	// Print the QR code
+	pngData, err := qrCode.PNG(256)
+	if err != nil {
+		return err
+	}
+	// Write PNG data to a file
+	file, err := os.Create("qrcode.png")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(pngData)
+	if err != nil {
+		return err
+	}
+	return nil
 }
