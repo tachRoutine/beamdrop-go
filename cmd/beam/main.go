fa6c5edc001c22000b167d5985b16365d2307073
Tachera Sasi <tacherasasi@gmail.com>
2025-09-05 12:34:01 +0300

    Implement QR code generation in main function

--- cmd/beam/main.go
+++ cmd/beam/main.go
@@ -8,6 +8,14 @@ import (
 )
 
 func main() {
+	data := "ekilie.com"
+	filename := "qrcode.png"
+	err := generateQRCode(data, filename)
+	if err != nil {
+		fmt.Println("Error generating QR code:", err)
+		return
+	}
+	fmt.Println("QR code generated and saved to", filename)
 	
 }
 
