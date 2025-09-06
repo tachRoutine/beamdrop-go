c98ef315d3cf468b78523ffb468ebc2613268609
Tachera Sasi <tacherasasi@gmail.com>
2025-09-06 22:56:50 +0300

    Remove debug print statements from init function in static package

--- static/static.go
+++ static/static.go
@@ -2,13 +2,11 @@ package static
 
 import (
 	"embed"
-	"fmt"
 )
 
 //go:embed all:frontend*
 var FrontendFiles embed.FS
 
-func init(){
-	fmt.Println("Static package initialized")
-	fmt.Println("Embedded files:", FrontendFiles)
+func init() {
+	//TODO: im gonna do something here later not sure what yet
 }
